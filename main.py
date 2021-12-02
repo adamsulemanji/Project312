3d8ba79ffc3608c2464374df2689a199f951aaf5
Adam Sulemanji <adamsulemanji@client-10-229-208-225.tamulink.tamu.edu>
2021-12-01 21:22:56 -0600

    partial main file

--- main.py
+++ main.py
@@ -5,13 +5,40 @@ import set
 import os
 import sys
 
-
+def print_menu():
+  print("*** Cache simulator menu ***",'\n',
+  "type one command:", '\n',
+  "1. cache-read", '\n',
+  "2. cache-write",'\n',
+  "3. cache-flush",'\n',
+  "4. cache-view",'\n',
+  "5. memory-view",'\n',
+  "6. cache-dump",'\n',
+  "7. memory-dump",'\n',
+  "8. quit",'\n',
+  "****************************", end = "\n")
 
 
 def main():
-  print("Hello World")
+
+
+
+  
+
   c = cache.Cache()
   c.initialize_memory(sys.argv[1])
+
+  val = ""
+
+
+  while val != "quit":
+    val = input("type one command: ")
+    
+    if val == "cache-read":
+      c.cache_read(input("type one command: ").split()[1])
+    
+    elif val == "cache-write":
+      
   c.memory_view()
   c.configure_cache()
   
